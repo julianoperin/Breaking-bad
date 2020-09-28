a0b684a93877a8676f25680566a7a29e5bfeb40a
‘Juliano <julianoperins@gmail.com>
2020-09-28 17:52:17 -0300

    Installed axios and made the first call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Header from "./components/ui/Header";
 
 function App() {
-  return <div className="App"></div>;
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result);
+    };
+
+    fetchItems();
+  }, []);
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
